fc51842802556d607947b3255393e4f15a8d9f40
Hoto Cocoa (Lee Sung Jae) <cocoa@hoto.us>
2017-04-03 00:45:22 +0900

    wat..

--- mail-sender/include.php
+++ mail-sender/include.php
@@ -3,12 +3,6 @@ namespace osu_petit;
 require __DIR__.'/../vendor/autoload.php';
 $config = require_once __DIR__.'/../config.php';
 
-$db_host = $config['db_host'];
-$db_user = $config['db_user'];
-$db_pass = $config['db_pass'];
-$db_name = $config['db_name'];
-$db_table = $config['db_table'];
-
 use Mailgun\Mailgun;
 $mg = new Mailgun($config['mailgun_key']);
 class Mailer {
