50f73eccc246b5ebcc3db0fcac9990141eb1b796
Hoto Cocoa (Lee Sung Jae) <cocoa@hoto.us>
2017-04-02 18:06:14 +0900

    clean style

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity=sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN crossorigin="anonymous">
-<form style="margin-top:10px!important;" action="register.php" method="post">
+<form action="register.php" method="post">
 	<center>
-	<input style="width:100px!important;" placeholder="osu! Username" name="username" required/>
+	<input placeholder="osu! Username" name="username" required/>
 	<label class="radio-inline"><input type="radio" name="mode" value="osu">osu!</label>
 	<label class="radio-inline"><input type="radio" name="mode" value="taiko">Taiko</label>
 	<label class="radio-inline"><input type="radio" name="mode" value="ctb">CatchTheBeat</label>
